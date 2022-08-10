98bee632d4d3f8d74b9da93450e1ef427cb56c57
Jagadesh <jagadesh.julapalli@gmail.com>
2022-08-10 13:34:45 +0530

    Added singleton with lazy

---
+++ DesignPatterns/Creational/Singleton/SingletonLazy/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Creational.Singleton.SingletonLazy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallel.Invoke(
+                () => Method1(),
+                () => Method2());
+            Console.ReadKey();
+        }
+
+        private static void Method2()
+        {
+            var instance = Singleton.GetInstance;
+            instance.WriteMessage("I am main2");
+        }
+
+        private static void Method1()
+        {
+            var instance = Singleton.GetInstance;
+            instance.WriteMessage("I am main1");
+        }
+    }
+}
---
+++ DesignPatterns/Creational/Singleton/SingletonLazy/Singleton.cs
@@ -0,0 +1,27 @@
+﻿namespace Creational.Singleton.SingletonLazy
+{
+    public sealed class Singleton
+    {
+        private readonly int _count = 0;
+        private Singleton()
+        {
+            _count++;
+            Console.WriteLine($"Instance count {_count}");
+        }
+        //private static Singleton? instance = new();
+        private static readonly Lazy<Singleton>? instance = new(()=>new Singleton());
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine($"Your message {message}");
+        }
+    }
+}
