bf6369b545e269eafae18cdcb13274b0dc934111
Jagadesh <jagadesh.julapalli@gmail.com>
2022-08-10 12:44:07 +0530

    Added Singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Parallel.Invoke(
+                ()=> Method1(), 
+                ()=> Method2());
+            Console.ReadKey();
+        }
+
+        private static void Method2()
+        {
+            var instance = Singleton.GetInstance;
+            instance.WriteMessage("I am main2");
+        }
+
+        private static void Method1()
+        {
+            var instance = Singleton.GetInstance;
+            instance.WriteMessage("I am main1");
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private readonly int _count = 0;
+        private Singleton()
+        {
+            _count++;
+            Console.WriteLine($"Instance count {_count}");
+        }
+        private static Singleton? instance = null;
+        private static readonly object instanceLock = new();
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (instanceLock)
+                    {
+                        if (instance == null)
+                            instance = new Singleton();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine($"Your message {message}");
+        }
+    }
+}
